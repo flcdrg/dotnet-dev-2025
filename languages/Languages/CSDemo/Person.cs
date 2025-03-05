d02e48a9252b5479d5e37172620a41a2e8ab93cf
David Gardiner <david@gardiner.net.au>
2025-03-06 08:56:25 +1030

    Add Languages

---
+++ languages/Languages/CSDemo/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace CSDemo;
+
+public class Person
+{
+    private int _age;
+
+    public int Age
+    {
+        get => _age;
+        set => _age = value;
+    }
+
+    public string Name { get; set; }
+}
---
+++ languages/Languages/CSDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CSDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            var firstName = "John";
+            person.Name = firstName;
+            person.Age = 30;
+
+            for (int i = 0; i < 10; i++)
+            {
+                person.Age++;
+                Console.WriteLine($"Age: {person.Age}");
+            }
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
